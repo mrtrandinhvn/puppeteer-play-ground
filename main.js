86ae799d299c61c8537ee41f647e767493e9b228
DinhTran <mrtrandinhvn@gmail.com>
2020-07-18 22:16:07 +0700

    Crawl audio from page

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
+const urlToCrawl = 'https://sachtienganhhanoi.com/audio-cambridge-global-english-1-cd-2-2/';
+const downloadFolder = ".\\audio2";
 (async () => {
     console.log("let the fun begin")
     const browser = await puppeteer.launch({
-        headless: false,
-        devtools: true,
+        headless: true,
+        devtools: false,
     });
-    try {
-        const page = await browser.newPage();
-        await page.goto('https://sachtienganhhanoi.com/audio-cambridge-global-english-1-2-cd-audio/?fbclid=IwAR38gFOMsss2d-5EiCCz940T32s36MD05jBX596Seoy6dkvP1ITaRU3mTrk', { waitUntil: 'networkidle2' });
-        await page.waitForSelector("li.wpcp__playlist-selector-list-item", { timeout: 60000 });
+    const page = await browser.newPage();
+    await page.goto(urlToCrawl, { waitUntil: 'networkidle2' });
+    await page.evaluate(() => {
+        document.getElementById("ShareoneDrive").scrollIntoView();
+    });
+    const crawlAudioPromise = new Promise((resolve) => {
+        console.log("Waiting for responses...")
+        page.on('response', async (response) => {
+            const request = response.request();
+            const requestBody = request.postData();
+            if (!requestBody) {
+                return;
+            }
 
-        const items = await page.evaluate(() => {
-            return document.querySelectorAll("li.wpcp__playlist-selector-list-item");
+            if (requestBody.indexOf("action=shareonedrive-get-playlist") >= 0) {
+                const fileInfos = (await response.json());
+                // const fileInfos = (await response.json()).slice(0, 1);
+
+                for (let i = 0; i < fileInfos.length; i++) {
+                    const fileInfo = fileInfos[i];
+                    const fileIndex = `[${i + 1}/${fileInfos.length}]`;
+                    console.log(`Downloading ${fileIndex}`);
+                    const bufferString = await downloadAsBinaryString(page, fileInfo);
+                    console.log("Begin writing to local directory...")
+                    writeFileSync(`${downloadFolder}\\${fileInfo.title}.mp3`, bufferString);
+                    console.log(`Finish writing file ${fileIndex}to local directory...`);
+                    console.log("======================================");
+                }
+
+                console.log(`Download completed. `)
+                resolve();
+            }
         });
-        console.log("items:", items[0]);
-    } catch (e) {
-        console.log("oops")
-        console.log(e);
-    }
-    finally {
-        console.log("closing browser")
-        await browser.close();
-    }
+    });
+
+    // await page.waitFor(10000);
+    await crawlAudioPromise;
+    console.log("closing browser")
+    await browser.close();
 })();
+
+async function downloadAsBinaryString(page, fileInfo) {
+    const bufferString = await page.evaluate(async (fileUrl) => {
+        const fileResponse = await fetch(fileUrl);
+        const blob = await fileResponse.blob();
+        const reader = new FileReader();
+        reader.readAsBinaryString(blob);
+        return new Promise(async resolve => {
+            reader.onload = () => resolve(reader.result);
+        });
+    }, fileInfo.source);
+
+    return bufferString;
+}
+
+function writeFileSync(fileName, bufferString) {
+    const buffer = Buffer.from(bufferString, 'binary');
+    fs.writeFileSync(fileName, buffer);
+}
